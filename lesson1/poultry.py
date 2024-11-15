510da908a127be5a25fcf3838e8ea2e69496599d
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-15 14:42:17 -0800

    Add prediction scatterplot

--- lesson1/poultry.py
+++ lesson1/poultry.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from ydata_profiling import ProfileReport
+import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.regression.linear_model import RegressionResultsWrapper
 
 from util.fetch import fetch_csv_dataset
 from util.temp_folder import TEMP
@@ -21,14 +24,43 @@ def get_poultry_data() -> pd.DataFrame:
     return pd.DataFrame(df[list(d.values())])
 
 
-def main() -> None:
-    df = get_poultry_data()
-    df["wt_gain"] = df.final_wt - df.initial_wt
-    print(df.describe())
-    print(df.dtypes)
+def create_ydata_profile_report(df: pd.DataFrame) -> None:
+    from ydata_profiling import ProfileReport
+
     print(df)
     report = ProfileReport(df)
     report.to_file(TEMP / "poultry_report.html")
+
+
+def ols_regression(x: pd.Series, y: pd.Series) -> RegressionResultsWrapper:
+    model = sm.OLS(y, x).fit()
+    assert isinstance(model, RegressionResultsWrapper)
+    return model
+
+
+def main(*, want_profile: bool = False) -> None:
+    df = get_poultry_data()
+    df["wt_gain"] = df.final_wt - df.initial_wt
+    if want_profile:
+        create_ydata_profile_report(df)
+
+    df = df.drop(columns=["id"])
+    sns.pairplot(df)
+
+    model = ols_regression(df.feed_wt, df.wt_gain)
+    df["predicted_gain"] = model.predict(df.feed_wt)
+
+    # Reshape from wide to long format.
+    df_long = df.melt(
+        id_vars=["feed_wt"],
+        value_vars=["wt_gain", "predicted_gain"],
+        var_name="gain_type",
+        value_name="gain",
+    )
+    ax = sns.scatterplot(data=df_long, x="feed_wt", y="gain", hue="gain_type")
+    ax.set_xlim(0, None)
+    ax.set_ylim(0, None)
+    plt.show()
 
 
 if __name__ == "__main__":
