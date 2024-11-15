a2fa30ce841268c4ab5000da30e2c88d124fceae
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-15 13:37:20 -0800

    Add a weight gain feature

--- lesson1/poultry.py
+++ lesson1/poultry.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from ydata_profiling import ProfileReport
 
 from util.fetch import fetch_csv_dataset
+from util.temp_folder import TEMP
 
 POULTRY_URL = (
     "https://github.com/darheman/project-feed-conversion-ratio-optimization"
@@ -12,20 +14,21 @@ def get_poultry_data() -> pd.DataFrame:
     d = {
         "Bird ID": "id",
         "Initial Weight (Grams)": "initial_wt",
-        "Final Weight (Grams)": "final_wt",
         "Feed Intake (Grams/Week)": "feed_wt",
+        "Final Weight (Grams)": "final_wt",
     }
     df = fetch_csv_dataset(POULTRY_URL).rename(columns=d)
-    df = df[list(d.values())]
-    assert isinstance(df, pd.DataFrame)
-    return df
+    return pd.DataFrame(df[list(d.values())])
 
 
 def main() -> None:
     df = get_poultry_data()
+    df["wt_gain"] = df.final_wt - df.initial_wt
     print(df.describe())
     print(df.dtypes)
     print(df)
+    report = ProfileReport(df)
+    report.to_file(TEMP / "poultry_report.html")
 
 
 if __name__ == "__main__":
