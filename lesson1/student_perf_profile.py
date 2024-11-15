1eded2c9af9e3c0dea8ae450c388ca3e82c747f2
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-15 12:11:22 -0800

    Add clean_student_dataframe

--- lesson1/student_perf_profile.py
+++ lesson1/student_perf_profile.py
@@ -1,17 +1,66 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from ydata_profiling import ProfileReport
+import seaborn as sns
 
 from util.temp_folder import TEMP
 
 
-def student_performance_report() -> None:
-    df = pd.read_csv(TEMP / "student-mat.csv", sep=";")
-    print(df)
-    print(df.dtypes)
+def student_performance_report(df: pd.DataFrame) -> None:
+    # This can take ~ ten seconds to import, so only do it when needed.
+    from ydata_profiling import ProfileReport
+
     print(df.describe())
-    prof = ProfileReport(df)
-    prof.to_file(str(TEMP / "student_performance_math_report.html"))
+    report = ProfileReport(df)
+    report.to_file(TEMP / "student_performance_math_report.html")
+
+
+def plot_correlations(df: pd.DataFrame) -> None:
+    df = clean_student_dataframe(df)
+    print(df)
+    sns.heatmap(df.corr(), annot=True)
+    sns.jointplot(x="G1", y="G3", data=df, kind="reg")
+    sns.jointplot(x="G2", y="G3", data=df, kind="reg")
+    plt.show()
+
+
+def clean_student_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(
+        columns=[
+            "Fjob",
+            "Mjob",
+            "Pstatus",
+            "address",
+            "famsize",
+            "guardian",
+            "reason",
+            "school",
+            "sex",
+        ]
+    )
+
+    pd.set_option("future.no_silent_downcasting", True)
+    y_or_n_cols = [
+        "activities",
+        "famsup",
+        "higher",
+        "internet",
+        "nursery",
+        "paid",
+        "romantic",
+        "schoolsup",
+    ]
+    for col in y_or_n_cols:
+        df[col] = df[col].replace({"yes": 1, "no": 0})
+
+    df = df.infer_objects(copy=False)
+    return df
+
+
+def main() -> None:
+    df = pd.read_csv(TEMP / "student-mat.csv", sep=";")
+    # student_performance_report(df)
+    plot_correlations(df)
 
 
 if __name__ == "__main__":
-    student_performance_report()
+    main()
